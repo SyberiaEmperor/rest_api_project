9cefe830cf2a334ce8e50a8bba4a3aa4e77f7e18
SyberiaImperor <Al.B.danilenko@gmail.com>
2022-09-12 21:53:36 +0300

    handler

--- cmd/main.go
+++ cmd/main.go
@@ -4,12 +4,14 @@ import (
 	"log"
 
 	restwebprj "github.com/SyberiaEmperor/rest_web_prj"
+	"github.com/SyberiaEmperor/rest_web_prj/pkg/handler"
 )
 
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(restwebprj.Server)
 
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000",handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
 }
