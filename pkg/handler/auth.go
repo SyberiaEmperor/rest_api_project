9ec32032320af8b6e982111e79af95705c633bdf
SyberiaImperor <Al.B.danilenko@gmail.com>
2022-09-19 19:39:52 +0300

    migration

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -2,10 +2,10 @@ package handler
 
 import "github.com/gin-gonic/gin"
 
-func (h *Handler) signUp(c *gin.Context){
+func (h *Handler) signUp(c *gin.Context) {
 
 }
 
-func (h *Handler) signIn(c *gin.Context){
-	
+func (h *Handler) signIn(c *gin.Context) {
+
 }
